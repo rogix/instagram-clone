6bcf1b14c4c2daf0283323012aece03c7872a52e
Roger <12768403+rogix@users.noreply.github.com>
2019-06-12 23:52:09 -0300

    configurando feed real time

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Feed.css';
 import api from '../services/api';
+import io from "socket.io-client";
 
 import more from '../assets/more.svg';
 import like from '../assets/like.svg';
@@ -13,9 +14,31 @@ class Feed extends Component {
     }
 
     async componentDidMount() {
+        this.registerToSocket();
+
         const response = await api.get('posts');
 
         this.setState({ feed: response.data })
+    }
+
+    registerToSocket = () => {
+        const socket = io('http://localhost:3333');
+
+        socket.on('post', newPost => {
+            this.setState({ feed: [newPost, ...this.state.feed] });
+        })
+
+        socket.on('like', likedPost => {
+            this.setState({
+                feed: this.state.feed.map(post =>
+                    post._id === likedPost._id ? likedPost : post
+                )
+            })
+        })
+    }
+
+    handleLike = id => {
+        api.post(`/posts/${id}/like`);
     }
 
     render() {
@@ -36,7 +59,9 @@ class Feed extends Component {
     
                         <footer>
                             <div className="actions">
-                                <img src={like} alt="" />
+                                <button type="button" onClick={() => this.handleLike(post._id)}>
+                                    <img src={like} alt="" />
+                                </button>
                                 <img src={comment} alt="" />
                                 <img src={send} alt="" />
                             </div>
