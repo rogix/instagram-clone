11095ed964fce53d3c03fd0e36267866db7fd0cb
Roger <12768403+rogix@users.noreply.github.com>
2019-06-12 22:59:47 -0300

    axios adicionado para feed

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Feed.css';
+import api from '../services/api';
 
 import more from '../assets/more.svg';
 import like from '../assets/like.svg';
@@ -7,64 +8,48 @@ import comment from '../assets/comment.svg';
 import send from '../assets/send.svg';
 
 class Feed extends Component {
+    state = {
+        feed: [],
+    }
+
+    async componentDidMount() {
+        const response = await api.get('posts');
+
+        this.setState({ feed: response.data })
+    }
+
     render() {
         return(
             <section id="post-list">
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Rogerio</span>
-                            <span className="place">Rio de Janeiro</span>
-                        </div>
-
-                        <img src={more} alt="Mais"/>
-                    </header>
-
-                    <img src="http://localhost:3333/files/wallpaper.jpg" alt="Imagem"/>
-
-                    <footer>
-                        <div className="actions">
-                            <img src={like} alt="" />
-                            <img src={comment} alt="" />
-                            <img src={send} alt="" />
-                        </div>
-
-                        <strong>800 curtidas</strong>
-
-                        <p>
-                            Um novo post
-                            <span>#react #omnistack</span>
-                        </p>
-                    </footer>
-                </article>
-
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Rogerio</span>
-                            <span className="place">Rio de Janeiro</span>
-                        </div>
-
-                        <img src={more} alt="Mais"/>
-                    </header>
-
-                    <img src="http://localhost:3333/files/wallpaper.jpg" alt="Imagem"/>
-
-                    <footer>
-                        <div className="actions">
-                            <img src={like} alt="" />
-                            <img src={comment} alt="" />
-                            <img src={send} alt="" />
-                        </div>
-
-                        <strong>800 curtidas</strong>
-
-                        <p>
-                            Um novo post
-                            <span>#react #omnistack</span>
-                        </p>
-                    </footer>
-                </article>
+                { this.state.feed.map(post => (
+                    <article>
+                        <header>
+                            <div className="user-info">
+                                <span>{post.author}</span>
+                                <span className="place">{post.place}</span>
+                            </div>
+    
+                            <img src={more} alt="Mais"/>
+                        </header>
+    
+                        <img src={`http://localhost:3333/files/${post.image}`} alt="Imagem"/>
+    
+                        <footer>
+                            <div className="actions">
+                                <img src={like} alt="" />
+                                <img src={comment} alt="" />
+                                <img src={send} alt="" />
+                            </div>
+    
+                            <strong>{post.likes} curtidas</strong>
+    
+                            <p>
+                                {post.description}
+                                <span>{post.hashtags}</span>
+                            </p>
+                        </footer>
+                    </article>
+                )) }
             </section>
         )
     }
