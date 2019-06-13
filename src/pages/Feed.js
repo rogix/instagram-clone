8e5f59a2bdab2bcc0ff5e68a72cf8d8520b8fb9c
Roger <12768403+rogix@users.noreply.github.com>
2019-06-12 22:43:56 -0300

    feed component

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,9 +1,71 @@
 import React, { Component } from 'react';
+import './Feed.css';
+
+import more from '../assets/more.svg';
+import like from '../assets/like.svg';
+import comment from '../assets/comment.svg';
+import send from '../assets/send.svg';
 
 class Feed extends Component {
     render() {
         return(
-            <h1>Feed</h1>
+            <section id="post-list">
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Rogerio</span>
+                            <span className="place">Rio de Janeiro</span>
+                        </div>
+
+                        <img src={more} alt="Mais"/>
+                    </header>
+
+                    <img src="http://localhost:3333/files/wallpaper.jpg" alt="Imagem"/>
+
+                    <footer>
+                        <div className="actions">
+                            <img src={like} alt="" />
+                            <img src={comment} alt="" />
+                            <img src={send} alt="" />
+                        </div>
+
+                        <strong>800 curtidas</strong>
+
+                        <p>
+                            Um novo post
+                            <span>#react #omnistack</span>
+                        </p>
+                    </footer>
+                </article>
+
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Rogerio</span>
+                            <span className="place">Rio de Janeiro</span>
+                        </div>
+
+                        <img src={more} alt="Mais"/>
+                    </header>
+
+                    <img src="http://localhost:3333/files/wallpaper.jpg" alt="Imagem"/>
+
+                    <footer>
+                        <div className="actions">
+                            <img src={like} alt="" />
+                            <img src={comment} alt="" />
+                            <img src={send} alt="" />
+                        </div>
+
+                        <strong>800 curtidas</strong>
+
+                        <p>
+                            Um novo post
+                            <span>#react #omnistack</span>
+                        </p>
+                    </footer>
+                </article>
+            </section>
         )
     }
 }
